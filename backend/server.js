0213442433a577e1fbdd7237648717de8807cba6
kolasyam <syammanikantakola426@gmail.com>
2024-06-17 14:29:22 +0530

    update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import authRoutes from './routes/auth.routes.js';
+import authRoutes from './routes/auth.route.js';
 import dotenv from "dotenv";
 import {v2 as cloudinary} from "cloudinary"
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js"
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js"
 dotenv.config();
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
